3aacee360dbfdf14ef7970d9ce8d7214eb4156e1
HB <hbianan@gmail.com>
2019-09-06 01:26:55 -0400

    Updates

---
+++ precache-manifest.54e10f228d47f2e8bdd89d8583a98d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8cef63db7b8c884604d17c005b30b3",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "65916bf80f0e32b064b7",
+    "url": "/ClickyGame/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "981e2003b55b563864c2",
+    "url": "/ClickyGame/static/js/2.ed9775e6.chunk.js"
+  },
+  {
+    "revision": "65916bf80f0e32b064b7",
+    "url": "/ClickyGame/static/js/main.885a42aa.chunk.js"
+  },
+  {
+    "revision": "199f94108222007da0e2",
+    "url": "/ClickyGame/static/js/runtime~main.04b08b17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
