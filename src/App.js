a53efd3bf57f4a4c4d1420fadcef057bd4135fcd
HB <hbianan@gmail.com>
2019-09-07 18:49:08 -0400

    files updated
    -shufffling images when clicked is now working
    -scoring is still on process

--- src/App.js
+++ src/App.js
@@ -3,44 +3,44 @@ import data from './data.json';
 import CharacterCard from './components/Cards';
 import Container from './components/Container';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <div className="row">
-//         <div className="col-12 col-md-4 col-lg-3"></div>
-//       </div>
-//     </div>
-//   );
-// }
+var shuffle = require('shuffle-array');
 
 class App extends Component {
   state = {
-    data
+    data,
+    currentScore : 0,
+    topScore : 0,
+    clicked: []
   };
 
-  disablePhoto = id => {
-    const img = this.state.data.filter(data => data.id !== id);
-    this.setState({ data });
-  }
+  scoreCount = id => {
+    let clicked = this.state.clicked;
+    // console.log(this.state.data)
+    console.log('image clicked')
+    // shuffle(this.state.data)
+    if(clicked.indexOf(id) === -1){
+      clicked.push(id)
+      this.setState({ data: shuffle(data) });
+      console.log(shuffle(this.state.data))
+    }
+  };
 
   render(){
     return (
       <div className="App container">
-        
-
         <Container>
-        <div className="row">
-        {this.state.data.map(character => (
-          <CharacterCard
-            key={character.id}
-            name={character.name}
-            image={character.image}
-          />
-        ))}
-        </div>
-
+          <div className="row">
+            {this.state.data.map(character => (
+              <CharacterCard
+                key={character.id}
+                scoreCount={this.scoreCount}
+                id={character.id}
+                name={character.name}
+                image={character.image}
+              />
+            ))}
+          </div>
         </Container>
-        
       </div>
 
     );
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -3,9 +3,9 @@ import './style.css';
 
 const CharacterCard = x => {
      return (
-          <div class="card text-center" style={{width: 18 + 'rem'}}>
-               <div class="card-body" id={x.id}>
-                    <h5 class="card-title">{x.name}</h5>
+          <div className="card text-center" onClick={() => x.scoreCount(x.id)} style={{width: 18 + 'rem'}}>
+               <div className="card-body" id={x.id}>
+                    <h5 className="card-title">{x.name}</h5>
                     <img alt={x.name} src={x.image}/>
                </div>
           </div>
